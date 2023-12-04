0fa2d2abb51d0ea5b92483c71641fe6de612e8b6
dmurillod <diegoa.murillod@gmail.com>
2023-12-04 16:58:21 -0500

    Se agregó la ruta quote(API)

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+import requests
 
+api_url = 'https://api.kanye.rest/'
+
+def get_quote():
+    response = requests.get(api_url)
+    result = response.json()['quote']
+    return result
 
 app = Flask(__name__)
 
@@ -14,6 +21,11 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/kanyequote')
+def quote():
+    quote = get_quote()
+    return render_template('quote.html',quote= quote)
 
 if __name__ == '__main__':
     app.run(debug= True)
